3bbf4d8d89494c1cd20452cf1db49287734cf462
Chris <clj@ollure.com>
2019-06-13 18:04:22 -0500

    build modules

--- 13 - JavaScript Modules and Using npm/es6modules/app.js
+++ 13 - JavaScript Modules and Using npm/es6modules/app.js
@@ -1,3 +1,12 @@
 import { uniq } from 'lodash';
 import insane  from 'insane';
 import jsonp from 'jsonp';
+import {apiKey, url, sayHi, age, dog } from './src/config';
+console.log(apiKey, url);
+
+sayHi('wes');
+
+const ages = [1,1111,4,42,12,7, 99];
+
+console.log(uniq(ages));
+
--- 13 - JavaScript Modules and Using npm/es6modules/webpack.config.js
+++ 13 - JavaScript Modules and Using npm/es6modules/webpack.config.js
@@ -2,35 +2,10 @@ const webpack = require('webpack');
 const nodeEnv = process.env.NODE_ENV || 'production';
 
 module.exports = {
-	devtool: 'source-map',
-	entry: {
-		filename: './app.js'	
-	},
-	output: {
-		filename: '_build/bundle.js'
-	},
-	module: {
-		loaders:[
-		{
-			test: /\.js$/,
-			exclude: /node_modules/,
-			loader: 'babel',
-			query: {
-				presets: ['es2016-native-modules']
-			}
-		}
-	  ]
-	},
-	plugins: [
-	//ulgify js
-	new webpack.optimize.UglifyJsPlugin({
-		compress:{ warnings: false },
-		output: { comments: false },
-		sourceMap: true
-	}),
-// env plugin
-	new webpack.DefinePlugin({
-		'_process.env_': { NODE_ENV: JSON.stringify(nodeEnv)}
-	})
-	]
-}
+  mode: process.env.NODE_ENV || 'production',
+  devtool: 'source-map',
+  entry: './app.js',
+  output: {
+    filename: 'bundle.js',
+  },
+};
