34eeb51fa2ab3afbe15e79fee34273bdae886fc8
Chris <clj@ollure.com>
2019-06-14 15:07:30 -0500

    app.js

--- 13 - JavaScript Modules and Using npm/es6modules/app.js
+++ 13 - JavaScript Modules and Using npm/es6modules/app.js
@@ -1,12 +1,21 @@
 import { uniq } from 'lodash';
 import insane  from 'insane';
 import jsonp from 'jsonp';
-import {apiKey, url, sayHi, age, dog } from './src/config';
-console.log(apiKey, url);
+import {apiKey as key, url, sayHi, age, dog } from './src/config';
 
-sayHi('wes');
+ import User, {createURL, gravatar} from './src/user';
 
-const ages = [1,1111,4,42,12,7, 99];
 
-console.log(uniq(ages));
+const wes = new User('wes bos', 'wes@email.com', 'wesbos.com');
+//console.log(wes);
+const profile = createURL(wes.name);
+console.log(profile);
+const image = gravatar(wes.email);
+console.log(image);
 
+//* console.log(key, url);
+//* sayHi('wes');
+
+//* const ages = [1,1111,4,42,12,7, 99];
+
+// console.log(uniq(ages));
